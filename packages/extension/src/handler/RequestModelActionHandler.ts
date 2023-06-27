a0a3d12d1d3b317bff6866cc386b62b208cbb3a8
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-06-27 08:26:46 +0200

    Upgrade sprotty-vscode to v0.5.0

--- packages/extension/src/handler/RequestModelActionHandler.ts
+++
@@ -1,6 +0,0 @@
-import { RequestModelAction } from "sprotty-protocol";
-import { AbstractWebViewActionHandler } from "./AbstractWebViewActionHandler";
-
-export class RequestModelActionHandler extends AbstractWebViewActionHandler<RequestModelAction> {
-    readonly kind = RequestModelAction.KIND;
-}
--- packages/webview/src/main.ts
+++ packages/webview/src/main.ts
@@ -66,4 +66,4 @@ export class StatesSprottyStarter extends SprottyLspEditStarter {
     }
 }
 
-new StatesSprottyStarter();
+new StatesSprottyStarter().start();
